6d24308feded398dc113997774940120c6714290
Mirela De Giuli <mireladegiuli@gmail.com>
2020-05-16 00:57:44 -0300

    correções de sexta-feira 15/05

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -19,77 +19,40 @@ library.add(
     faEnvelope
 );
 
-//TypeScript do Angular do Grupo
-
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-sobre',
-//   templateUrl: './sobre.component.html',
-//   styleUrls: ['./sobre.component.css']
-// })
-// export class SobreComponent implements OnInit {
-
-//   pessoa: any = null;
-
-//   isMobile = false;
-//   getIsMobile(): boolean {
-//     const w = document.documentElement.clientWidth;
-//     const breakpoint = 1024;
-//     console.log(w);
-//     if (w <= breakpoint) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   constructor() { }
-
-
-//   ngOnInit(): void {
-
-//     this.isMobile = this.getIsMobile();
-//     window.onresize = () => {
-//       this.isMobile = this.getIsMobile();
-//     };
-
-
-//   }
-
-
-
-// }
-
-//HTML do Angular do Grupo
-
 export default class About extends Component {
 
+    // isMobile = false;
+    // getIsMobile()  boolean {
+    //   const w = document.documentElement.clientWidth;
+    //   const breakpoint = 1024;
+    //   console.log(w);
+    //   if (w <= breakpoint) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // }
+   
     render () {
-
-        // var nome = ['mirela', 'pedro', 'rafael', 'thiago'];
-
-        // escolhePessoa(nome){
-        //     this.props.nome;
-        // }
 
         return (
             <div className="estilo-geral">
-                <div className="texto-sobre">
+                {/*Texto de apresentação da equipe e do projeto*/}
+                <div className="texto-sobre"> 
                     <div className="container-sobre">
-                        <div className="explicação-proj">
+                        <div className="explicação-proj">                            
                             <h1>Sobre</h1>
-                            <p>Somos a 4-Front!</p>
+                            <p>Somos a <strong>4-Front!</strong></p>
                             <p>Uma equipe formada por quatro desenvolvedores que estão em busca de novos conhecimentos
                             e desafios profissionais. Cada integrante tem suas habilidades e áreas de especialidade, e unidos formamos um time
                             capacitado para desenvolvimento web completo. Nossos projetos priorizam a responsividade e a qualidade de interação
                             para que seu site seja acessível nos diferentes dispositivos de seus usuários. Nossos serviços contemplam desde o 
-                            design da interface até toda a programação Front-End, e fazemos aplicação de HTML, CSS e JavaScript tanto puros quanto 
-                            usando Bootstrap, Angular e React.                            
+                            design da interface até toda a programação Front-End. Fazemos aplicação de HTML, CSS e JavaScript puros, ou 
+                            com os frameworks Angular e React.                            
                             </p>
-                            {/*Explicação do trabalho proposto pela cocreare academy de um web site resposivo com integração com rede social,
-                            realizado pelo grupo composto por Carlos Alberto, Fabrício, Valéria e Wesley.
-                            A descrição do projeto será incrementatada conforme o andamento do projeto e novas camadas forem sendo adicionadas.*/}
+                            <p><strong>Este site é resultado de um dos projetos da Capacitação em Front-end da Cocreare Academy, que teve como
+                                objetivo o desenvolvimento de um site responsivo integrado com a API da rede social Twitter.
+                            </strong></p>
                         </div>
 
                         <div className="img-sobre">
@@ -107,9 +70,9 @@ export default class About extends Component {
                             <div className="mini-card">
                                     <img src={Mirela} alt="imagem perfil Mirela" className="img-perfil-icone"/>
                                     <p>Mirela De Giuli</p>
-                                    <a className="btn" onClick="this.escolhePessoa('mirela')">Saiba mais</a>
+                                    <a className="btn" onClick="">Saiba mais</a>
                             </div>
-
+                            
                             <div className="mini-card">
                                 <div>
                                     <img src={Pedro} alt="imagem perfil Pedro" className="img-perfil-icone"/>
@@ -142,13 +105,13 @@ export default class About extends Component {
                                     <img src={BackgroundCard} alt="background card" className="card-img"/>
                                     <img src={Mirela} alt="imagem perfil mirela" className="img-perfil-sobre"/>
                                     <h2>Mirela De Giuli</h2>
-                                    <p className="introducao">Oie! Sou a Mirela, designer de produto e desenvolvedora front-end Jr, tenho 24 anos e 
+                                    <p className="introducao">Sou a Mirela, designer de produto e desenvolvedora front-end Jr, tenho 24 anos e 
                                     moro em Bauru. Sou apaixonada por livros, café e inovação!</p>
 
                                     <ul className="redes-sociais">
-                                        <li><a href="https://github.com/mirelagiuli" target="_blank"><FontAwesomeIcon icon={faGithub} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="mailto:mireladegiuli@gmail.com"><FontAwesomeIcon icon={faEnvelope} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="https://www.linkedin.com/in/mirela-de-giuli" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{color: '#334459'}} size="3x"/></a></li>
+                                        <li><a href="https://github.com/mirelagiuli" target="_blank"><FontAwesomeIcon className="style-redes" icon={faGithub}/></a></li>
+                                        <li><a href="mailto:mireladegiuli@gmail.com"><FontAwesomeIcon className="style-redes" icon={faEnvelope}/></a></li>
+                                        <li><a href="https://www.linkedin.com/in/mirela-de-giuli" target="_blank"><FontAwesomeIcon className="style-redes" icon={faLinkedin}/></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -158,12 +121,12 @@ export default class About extends Component {
                                     <img src={BackgroundCard} alt="background card" className="card-img"/>
                                     <img src={Pedro} alt="imagem perfil pedro" className="img-perfil-sobre"/>
                                     <h2>Pedro H. Borges</h2>
-                                    <p className="introducao">Olá! Sou Pedro</p>
+                                    <p className="introducao">Sou Pedro</p>
 
                                     <ul className="redes-sociais">
-                                        <li><a href="https://github.com/pedrohrb7" target="_blank"><FontAwesomeIcon icon={faGithub} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="mailto:pedrohenriquer77@hotmail.com"><FontAwesomeIcon icon={faEnvelope} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="https://www.linkedin.com/in/pedrohrb/" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{color: '#334459'}} size="3x"/></a></li>
+                                        <li><a href="https://github.com/pedrohrb7" target="_blank"><FontAwesomeIcon className="style-redes" icon={faGithub}/></a></li>
+                                        <li><a href="mailto:pedrohenriquer77@hotmail.com"><FontAwesomeIcon className="style-redes" icon={faEnvelope}/></a></li>
+                                        <li><a href="https://www.linkedin.com/in/pedrohrb/" target="_blank"><FontAwesomeIcon className="style-redes" icon={faLinkedin}/></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -176,9 +139,9 @@ export default class About extends Component {
                                     <p className="introducao">Sou Rafael</p>
 
                                     <ul className="redes-sociais">
-                                        <li><a href="https://github.com/rafael-rodrigues-santos" target="_blank"><FontAwesomeIcon icon={faGithub} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="mailto:rafaelrodrigues_@outlook.com"><FontAwesomeIcon icon={faEnvelope} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="https://www.linkedin.com/in/rafael-rodrigues-santos/" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{color: '#334459'}} size="3x"/></a></li>
+                                        <li><a href="https://github.com/rafael-rodrigues-santos" target="_blank"><FontAwesomeIcon className="style-redes" icon={faGithub}/></a></li>
+                                        <li><a href="mailto:rafaelrodrigues_@outlook.com"><FontAwesomeIcon className="style-redes" icon={faEnvelope}/></a></li>
+                                        <li><a href="https://www.linkedin.com/in/rafael-rodrigues-santos/" target="_blank"><FontAwesomeIcon className="style-redes" icon={faLinkedin}/></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -188,12 +151,12 @@ export default class About extends Component {
                                     <img src={BackgroundCard} alt="background card" className="card-img"/>
                                     <img src={Thiago} alt="imagem perfil thiago" className="img-perfil-sobre"/>
                                     <h2>Thiago Dutra</h2>
-                                    <p className="introducao">Oi! Sou Thiago</p>
+                                    <p className="introducao">Sou Thiago</p>
 
                                     <ul className="redes-sociais">
-                                        <li><a href="https://github.com/thiagosilvadtr" target="_blank"><FontAwesomeIcon icon={faGithub} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="mailto:"><FontAwesomeIcon icon={faEnvelope} style={{color: '#334459'}} size="3x"/></a></li>
-                                        <li><a href="#" target="_blank"><FontAwesomeIcon icon={faLinkedin} style={{color: '#334459'}} size="3x"/></a></li>
+                                        <li><a href="https://github.com/thiagosilvadtr" target="_blank"><FontAwesomeIcon className="style-redes" icon={faGithub}/></a></li>
+                                        <li><a href="mailto:"><FontAwesomeIcon className="style-redes" icon={faEnvelope}/></a></li>
+                                        <li><a href="#" target="_blank"><FontAwesomeIcon className="style-redes" icon={faLinkedin}/></a></li>
                                     </ul>
                                 </div>
                             </div> 
