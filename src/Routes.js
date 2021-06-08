52c63477e52c5b0036d894e85fdc08b6c4856128
Pedro <phrbrock@gmail.com>
2021-06-08 10:01:15 -0300

    removido bootstrap/adicionado material-ui - reconfigurado rotas e app.js p/ app.jsx

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import HomeOld from "./pages/Home/HomeOld";
 import About from "./pages/About";
 import Notfound from "./pages/Notfound";
 
-import Home2 from "./pages/Home/Home";
+import Home2 from "./pages/Home/Home2";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/">
-        <Home />
+        <HomeOld />
       </Route>
 
       <Route exact path="/about">
