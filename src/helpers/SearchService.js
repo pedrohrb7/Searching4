6f8ecf043a1b0a9c8ffa4c2ece7c5f6494892e24
Pedro <phrbrock@gmail.com>
2021-06-14 01:16:50 -0300

    added getInitialSearch with random value to the service

--- src/helpers/SearchService.js
+++ src/helpers/SearchService.js
@@ -1,11 +1,42 @@
 import axios from "axios";
 
 const baseUrl =
-  "https://cors.bridged.cc/api.twitter.com:443/1.1/search/tweets.json?count=100&q=google";
+"https://cors.bridged.cc/api.twitter.com:443/1.1/search/tweets.json?count=50&q=";
+
+export const getInitialSearch = async () => {
+  const initialValue = [
+    "politic",
+    "sports",
+    "science",
+    "technology",
+    "health",
+    "covid",
+  ];
+  const randomValue =
+    initialValue[Math.floor(Math.random() * initialValue.length)];
+  console.log("Random --> ", randomValue);
+
+  try {
+    const api = axios.create({
+      baseUrl,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAAPiJJAEAAAAATjQf6fngCaxi618Ty6I%2BtXL5jDA%3D2J4ExwpJRRxDy8DIHfWgxqWiLsQflJyYbPDmv3jY2B0gw8iu7i",
+      },
+    });
+
+    const res = await api.get(`${baseUrl}${randomValue}`);
+    let tweets = res;
+    console.log("random search --> ", tweets.data.statuses);
+    return tweets;
+  } catch (err) {
+    console.log("Algo ocorreu initialSearch: -> " + err);
+  }
+};
 
 export const SearchService = async (props) => {
-  const { tweet } = props;
-
   let tweet = props;
   try {
     const api = axios.create({
@@ -14,15 +45,13 @@ export const SearchService = async (props) => {
       headers: {
         "Content-Type": "application/json",
         Authorization:
-          "Bearer AAAAAAAAAAAAAAAAAAAAAPiJJAEAAAAATjQf6fngCaxi618Ty6I%2BtXL5jDA%3D2J4ExwpJRRxDy8DIHfWgxqWiLsQflJyYbPDmv3jY2B0gw8iu7i",
+        "Bearer AAAAAAAAAAAAAAAAAAAAAPiJJAEAAAAATjQf6fngCaxi618Ty6I%2BtXL5jDA%3D2J4ExwpJRRxDy8DIHfWgxqWiLsQflJyYbPDmv3jY2B0gw8iu7i",
       },
     });
 
     const res = await api.get(`${baseUrl}${tweet}`);
-    let tweets= res;
+    let tweets = res;
     console.log("tweetData --> ", tweets.data.statuses);
     return tweets;
-  } catch (err) {
-    console.log("Algo ocorreu: -> " + err);
-  }
+  } catch (err) {console.log("Algo ocorreu: -> " + err);}
 };
